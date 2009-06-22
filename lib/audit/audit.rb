a3256d091e1b8012e093a84940a1b553c82383e2
Paul Campbell <paul@rslw.com>
2009-06-22 20:33:26 +0100

    add named routes

--- lib/audit/audit.rb
+++ lib/audit/audit.rb
@@ -1,4 +1,8 @@
 class Audit < ActiveRecord::Base
   belongs_to(:auditable, :polymorphic => true)
   belongs_to :user
+  
+  named_scope :created, :conditions => {:action => 'create'}
+  named_scope :updated, :conditions => {:action => 'update'}
+  named_scope :destroy, :conditions => {:action => 'destroy'}
 end
--- spec/audit_spec.rb
+++ spec/audit_spec.rb
@@ -19,6 +19,7 @@ describe Audit do
     @test_model.audits.count.should == 1
     @test_model.audits.first.action.should == 'create'
     @test_model.audits.first.auditable.should == @test_model
+    @test_model.audits.created.size.should == 1
   end
   
   it "should create an update audit on update" do
@@ -28,6 +29,8 @@ describe Audit do
     @test_model.audits.count.should == 2
     @test_model.audits.last.action.should == 'update'
     @test_model.audits.last.auditable.should == @test_model
+    @test_model.audits.created.size.should == 1
+    @test_model.audits.updated.size.should == 1
   end
   
   it "should create a delete audit on delete" do
@@ -36,5 +39,7 @@ describe Audit do
     @test_model.audits.count.should == 2
     @test_model.audits.last.action.should == 'destroy'
     @test_model.audits.last.auditable_id.should == @test_model.id
+    @test_model.audits.created.size.should == 1
+    @test_model.audits.destroyed.size.should == 1
   end
 end
